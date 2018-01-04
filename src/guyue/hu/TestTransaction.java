a9b323465a37d942a6c8b1d6c0a3d4f97b9b1aac
GuobaoHu <398456458@qq.com>
2018-01-04 16:06:24 +0800

    0.4 事物管理相关

---
+++ src/guyue/hu/TestTransaction.java
@@ -0,0 +1,54 @@
+package guyue.hu;
+
+import java.sql.*;
+
+public class TestTransaction {
+
+	public static void main(String[] args) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+		String user = "guyue";
+		String psw = "guyue";
+		Connection conn = null;
+		PreparedStatement stm = null;
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, psw);
+			
+			conn.setAutoCommit(false);
+			String sql = "insert into dept2 values (?, ?, ?)";
+			stm = conn.prepareStatement(sql);
+			stm.setInt(1, 1);
+			stm.setString(2, "guyue");
+			stm.setString(3, "guyue");
+			stm.addBatch();
+			
+			stm.setInt(1, 2);
+			stm.setString(2, "guyue");
+			stm.setString(3, "guyue");
+			stm.addBatch();
+			
+			stm.executeBatch();
+			conn.commit();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			try {
+				if(conn != null) {
+					conn.rollback();
+					conn.setAutoCommit(true);
+				}
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		} finally {
+			try {
+				if(stm != null) stm.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
