395920fa5d2cee134d5966a7f5740596c7d65fec
GuobaoHu <398456458@qq.com>
2018-01-05 16:42:55 +0800

    *附加 测试连接mySql

---
+++ src/guyue/hu/TestConnMysql.java
@@ -0,0 +1,38 @@
+package guyue.hu;
+
+import java.sql.*;
+
+public class TestConnMysql {
+
+	public static void main(String[] args) {
+		String driver = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/mydata";
+		String user = "root";
+		String psw = "root";
+		Connection conn = null;
+		Statement stm = null;
+		ResultSet rs = null;
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, psw);
+			stm = conn.createStatement();
+			rs = stm.executeQuery("select * from dept");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(stm != null) stm.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
