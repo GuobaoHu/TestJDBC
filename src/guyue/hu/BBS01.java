765d3c62c9b3f460a01ca7e0c7b9cb30af723e38
GuobaoHu <398456458@qq.com>
2018-01-06 11:04:05 +0800

    BBS0.2 优化，在单独的方法中建立与数据库的连接，不再在递归方法中建立连接，因为递归会多次调用，就会建立多个连接

--- src/guyue/hu/BBS01.java
+++ src/guyue/hu/BBS01.java
@@ -7,19 +7,45 @@ import java.sql.*;
  *
  */
 public class BBS01 {
+	private static String driver = "com.mysql.jdbc.Driver";
+	private static String url = "jdbc:mysql://localhost/bbs?user=root&password=root";
+	
 	public static void main(String[] args) {
-		new BBS01().launch(0, 0);
+		new BBS01().show();
 	}
 	
-	public void launch(int pid, int count) {
-		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost/bbs?user=root&password=root";
+	/**
+	 * 在本方法中建立与数据库的连接
+	 * 由于递归方法会多次调用，不适合在递归方法中连接数据库，否则会多次连接数据库，严重影响效率 
+	 */
+	public void show() {
 		Connection conn = null;
-		PreparedStatement stm = null;
-		ResultSet rs = null;
 		try {
 			Class.forName(driver);
 			conn = DriverManager.getConnection(url);
+			launch(conn, 0, 0);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 递归方法，求得父级下的所有子级
+	 * @param pid 父id，本方法求得pid下的所有子项
+	 * @param count 层级
+	 */
+	public void launch(Connection conn, int pid, int count) {
+		PreparedStatement stm = null;
+		ResultSet rs = null;
+		try {
 			String sql = "select * from article where pid = ?";
 			stm = conn.prepareStatement(sql);
 			stm.setInt(1, pid);
@@ -33,18 +59,15 @@ public class BBS01 {
 			while(rs.next()) {
 				System.out.println(prefix + rs.getString(5));
 				if(rs.getInt(7) == 1) {
-					launch(rs.getInt(1), count + 1);
+					launch(conn, rs.getInt(1), count + 1);
 				}
 			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				if(rs != null) rs.close();
 				if(stm != null) stm.close();
-				if(conn != null) conn.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
